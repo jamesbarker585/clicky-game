ac568ab30c4a2c4d56317ba0d0a7df202934c55c
james barker <jamesbarker585@gmail.com>
2019-02-28 15:06:13 -0700

    new app

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import './App.css';
+import aircraft from './aircraft.json'
+import Wrapper from './components/Wrapper'
+import Navpills from './components/Navpills'
+import Title from './components/Title'
+import AircraftCard from './components/AircraftCard'
+
+class App extends Component {
+    state = {
+        message: "Click an image to begin!",
+        topScore: 0,
+        curScore: 0,
+        aircraft: aircraft,
+        unselectedAircraft: aircraft
+    }
+
+    componentDidMount() {
+    }
+
+    shuffleArray = array => {
+        for (let i = array.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    }
+
+    selectaircraft = jet => {
+        const findAircraft = this.state.unselectedAircraft.find(item => item.jet === jet);
+
+        if(findAircraft === undefined) {
+            // failure to select a new aircraft
+            this.setState({ 
+                message: "You guessed incorrectly!",
+                topScore: (this.state.curScore > this.state.topScore) ? this.state.curScore : this.state.topScore,
+                curScore: 0,
+                aircraft: aircraft,
+                unselectedAircraft: aircraft
+            });
+        }
+        else {
+            // success to select a new aircraft
+            const newAircraft = this.state.unselectedAircraft.filter(item => item.jet !== jet);
+            
+            this.setState({ 
+                message: "You guessed correctly!",
+                curScore: this.state.curScore + 1,
+                aircraft: aircraft,
+                unselectedAircraft: newAircraft
+            });
+        }
+
+        this.shuffleArray(aircraft);
+    };
+
+    render() {
+        return (
+            <Wrapper>
+                <Navpills
+                    message={this.state.message}
+                    curScore={this.state.curScore}
+                    topScore={this.state.topScore}
+                />
+                <Title />
+                {
+                    this.state.aircrafts.map(aircraft => (
+                        <AircraftCard
+                            jet={aircraft.jet}
+                            image={aircraft.image}
+                            selectAircraft={this.selectAircraft} 
+                            curScore={this.state.curScore}
+                        />
+                    ))
+                }
+            </Wrapper>
+        );
+    }
+}
+
+export default App;
+
---
+++ src/components/AircraftCard/AircraftCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./AircraftCard.css";
+
+const DogCard = props => (
+    <div className="card">
+        <div className="img-container">
+            <a onClick={() => props.selectAircraft(props.jet)} 
+                className={props.curScore === 0 ? "style_prevu_kit style_prevu_kit_ex" : "style_prevu_kit"}
+            >
+                <img alt={props.jet} src={props.image} />
+            </a>
+        </div>
+    </div>
+);
+
+export default AircraftCard;
